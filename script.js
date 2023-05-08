f4747b3c90a263e6d6a333bd6a70a4346a491324
Majd_Mawas <majosmws@gmail.com>
2023-05-08 22:46:30 +0300

    add some features to the project

--- script.js
+++ script.js
@@ -10,11 +10,14 @@ function characterCount(){
     if(length > 280)
     {
         tweet_element.setAttribute('disabled', 1);
-        
+        tweet_element.classList.replace('btn-primary','btn-danger');
+        counter_element.classList.replace('text-bg-secondary','text-bg-danger');
     }
     else
     {
         tweet_element.removeAttribute('disabled');
+        tweet_element.classList.replace('btn-danger','btn-primary');
+        counter_element.classList.replace('text-bg-danger','text-bg-secondary');
     }
 }
 
